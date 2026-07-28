76e9bfc21614d493e976d1b18a2c85cfaa8208be
Adrija Mitra <mitraadrija571@gmail.com>
2026-07-28 20:05:15 +0530

    Add Bear and Big Brother solution

---
+++ Bear_and_big_brother.java
@@ -0,0 +1,19 @@
+//791A
+//Bear and big brother
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   int limak = sc.nextInt();
+	   int bob = sc.nextInt();
+	   int years =0;
+	   while(limak <= bob){
+	       limak = limak*3;
+	       bob = bob*2;
+	       years++;
+	   }
+	   System.out.println(years);
+	       
+	}
+}
