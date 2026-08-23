612507f00f55110774cb21e4899a2f3deb933896
Adrija Mitra <mitraadrija571@gmail.com>
2026-08-23 11:08:10 +0530

    Implement Diverse Substring solution in Java

---
+++ Diverse_Substring.java
@@ -0,0 +1,20 @@
+//1073A
+//Diverse substring
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String s = sc.next();
+		for(int i=0; i<n-1; i++){
+		    if(s.charAt(i) != s.charAt(i+1)){
+		        System.out.println("YES");
+		        System.out.println(""+s.charAt(i)+s.charAt(i+1));
+		        return;
+		    }
+		    
+		}
+		System.out.println("NO");
+	}
+}
