048c0fc33ff33556c232615633680d30d049db98
Adrija Mitra <mitraadrija571@gmail.com>
2026-08-25 10:18:11 +0530

    Add Java program to handle long words

---
+++ Way_too_long_words.java
@@ -0,0 +1,20 @@
+//71A
+//Way too long words
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t--!=0){
+		   String s = sc.next();
+		   int n= s.length();
+		   if(n<=10){
+		       System.out.println(s);
+		   }else{
+		       System.out.println(s.charAt(0)+""+(n-2)+""+s.charAt(n-1));
+		   }
+		}
+		
+	}
+}
