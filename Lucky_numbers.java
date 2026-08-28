cdc3c32e452b464769d0fa4d2f2223f4a428d680
Adrija Mitra <mitraadrija571@gmail.com>
2026-08-28 10:54:36 +0530

    Create Lucky_numbers.java

---
+++ Lucky_numbers.java
@@ -0,0 +1,13 @@
+//630C
+//Lucky numbers
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		long n= sc.nextLong();
+		long result = (1<<(n+1))-2;
+		System.out.println(result);
+		
+	}
+}
